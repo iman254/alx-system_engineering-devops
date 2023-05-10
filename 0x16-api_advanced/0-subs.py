c7f197c904b40744c484c01f15bb67ef3efbc676
iman254 <stephaniemarquinz@icloud.com>
2023-05-10 14:22:29 +0300

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A python function that querries reddit api and returns no of subs for a given subredit"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """returns the num of subs in a subreddit"""
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers ={"User-Agent": 'My Agent'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        no_subs = data.get("data").get("subscribers")
+        return no_subs
+    else:
+        return 0
