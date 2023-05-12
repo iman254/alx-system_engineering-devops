49b5b8e1c4f895ed56d2152edae3a9f792f82a2a
iman254 <stephaniemarquinz@icloud.com>
2023-05-12 15:26:05 +0300

    task

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""recursive function that returns a list containing titles
-of a hot article for a subreddit"""
-
+""" Function that queries the Reddit API and returns a list containing
+the titles of all hot articles for a given subreddit."""
 import requests
 
 
-def recurse(subreddit, after=None, hot_list=[]):
-    """list containing titles of all the hot articles for a given subreddit"""
+def recurse(subreddit, hot_list=[], after=None):
+    """returns None if no results are foundfor given subreddit. """
 
-    url = "https://www.reddit.com/r/{}/hot.json?limit=50&after={}".format(subreddit, after)
-    headers = {"User-agent": 'my agent'}
+    headers = {'User-agent': 'my agent'}
+    url = "https://www.reddit.com/r/{}/hot.json?limit=50&after={}".format(
+            subreddit, after)
     posts = requests.get(url, headers=headers, allow_redirects=False)
 
     if posts.status_code == 200:
