df18b57a7814171599c65e4122dda99883e73bb8
iman254 <stephaniemarquinz@icloud.com>
2023-05-20 04:23:33 +0300

    task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,8 @@
+# a puppet script thats able to replace or rewrite a line
+
+$file_path= '/var/ww/html/wp-setting.php'
+
+exet {'replace_line':
+    command => "sed -i s/phpp/php/g' $(file_path}",
+    path    => ['/bin','/usr/bin']
+}
